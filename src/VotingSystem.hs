c83665dc65a0db9b4069a592dbefe3c9eac0ca8b
lakiryt <lakiryt@github.com>
2019-11-28 23:44:24 +0100

    Implemented data type PrefProfile

--- src/VotingSystem.hs
+++ src/VotingSystem.hs
@@ -2,4 +2,9 @@ module VotingSystem
     ( VotingSystem
     ) where
 
+data PrefProfile c t = PP {
+        canditates :: [c],
+        votes :: [t]
+    }
+
 type VotingSystem c t = PrefProfile c t -> [c]
